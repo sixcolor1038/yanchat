fa2328ed007803acc2a5ddc20bdef48f64fad38e
sixcolor <sixcolor1038@163.com>
2024-07-14 20:34:43 +0800

    用户模块开发

--- yanchat-chat-server/src/main/java/com/yan/yanchat/common/user/service/impl/LoginServiceImpl.java
+++ yanchat-chat-server/src/main/java/com/yan/yanchat/common/user/service/impl/LoginServiceImpl.java
@@ -1,6 +1,8 @@
 package com.yan.yanchat.common.user.service.impl;
 
+import com.yan.yanchat.common.infrastructure.utils.JWTUtils;
 import com.yan.yanchat.common.user.service.LoginService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -10,8 +12,28 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class LoginServiceImpl implements LoginService {
+
+    @Autowired
+    private JWTUtils jwtUtils;
+
+    @Override
+    public boolean verify(String token) {
+        return false;
+    }
+
+    @Override
+    public void renewalTokenIfNecessary(String token) {
+
+    }
+
     @Override
     public String login(Long uid) {
+        String token = jwtUtils.createToken(uid);
+        return token;
+    }
+
+    @Override
+    public Long getValidUid(String token) {
         return null;
     }
 }
