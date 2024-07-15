4e5414d54873e48d6261e2e28baa1abfe0c7834d
sixcolor <sixcolor1038@163.com>
2024-07-15 11:37:28 +0800

    用户模块开发

---
+++ yanchat-chat-server/src/main/java/com/yan/yanchat/common/infrastructure/config/RedissonConfig.java
@@ -0,0 +1,30 @@
+package com.yan.yanchat.common.infrastructure.config;
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Author: sixcolor
+ * @Date: 2024-02-15 19:16
+ * @Description:
+ */
+@Configuration
+public class RedissonConfig {
+    @Autowired
+    private RedisProperties redisProperties;
+
+    @Bean
+    public RedissonClient redissonClient() {
+        Config config = new Config();
+        config.useSingleServer()
+                .setAddress("redis://" + redisProperties.getHost() + ":" + redisProperties.getPort())
+                .setPassword(redisProperties.getPassword())
+                .setDatabase(redisProperties.getDatabase());
+        return Redisson.create(config);
+    }
+}
