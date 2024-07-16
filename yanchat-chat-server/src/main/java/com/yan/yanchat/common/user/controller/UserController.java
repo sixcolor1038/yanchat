7df8384bb6b025dd6528aeba12a58d42db21c093
sixcolor <sixcolor1038@163.com>
2024-07-16 15:26:59 +0800

    用户模块开发

--- yanchat-chat-server/src/main/java/com/yan/yanchat/common/user/controller/UserController.java
+++ yanchat-chat-server/src/main/java/com/yan/yanchat/common/user/controller/UserController.java
@@ -1,8 +1,11 @@
 package com.yan.yanchat.common.user.controller;
 
-
+import com.yan.yanchat.common.user.domain.vo.resp.UserInfoResp;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -11,13 +14,21 @@ import org.springframework.web.bind.annotation.RestController;
  * @Description: 用户表 前端控制器
  */
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/capi/user")
+@Api(value = "用户模块接口")
 public class UserController {
 
     @GetMapping()
-    public String get() {
+    public String test() {
         System.out.println("啊呀呀");
         return "success";
+    }
+
+    @GetMapping("/userinfo")
+    @ApiOperation("获取用户信息")
+    public UserInfoResp getUserInfo(@RequestParam Long uid){
+
+        return null;
     }
 
 }
---
+++ yanchat-chat-server/src/main/java/com/yan/yanchat/common/user/domain/vo/resp/UserInfoResp.java
@@ -0,0 +1,25 @@
+package com.yan.yanchat.common.user.domain.vo.resp;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+/**
+ * @Author: sixcolor
+ * @Date: 2024-02-16 10:08
+ * @Description:
+ */
+@Data
+@ApiModel(value = "用户信息")
+public class UserInfoResp {
+    @ApiModelProperty(value = "用户id")
+    private Long id;
+    @ApiModelProperty(value = "用户名称")
+    private String name;
+    @ApiModelProperty(value = "用户头像")
+    private String avatar;
+    @ApiModelProperty(value = "用户性别")
+    private Integer sex;
+    @ApiModelProperty(value = "剩余改名次数")
+    private Integer modifyNameChance;
+}
