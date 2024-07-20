644a235e13c238e4d5b6d9ce566d749eec130694
sixcolor <sixcolor1038@163.com>
2024-07-20 18:16:25 +0800

    用户联系人模块开发

--- yanchat-chat-server/src/main/java/com/yan/yanchat/common/user/dao/UserFriendDao.java
+++ yanchat-chat-server/src/main/java/com/yan/yanchat/common/user/dao/UserFriendDao.java
@@ -1,12 +1,21 @@
 package com.yan.yanchat.common.user.dao;
 
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.yan.yanchat.common.user.domain.entity.UserFriend;
+import com.yan.yanchat.common.user.mapper.UserFriendMapper;
 import org.springframework.stereotype.Service;
 
 /**
- * @Author: sixcolor
- * @Date: 2024-02-18 15:20
+ * @Author: sixcolor 
+ * @Date: 2024-02-18
  * @Description:
  */
 @Service
-public class UserFriendDao {
+public class UserFriendDao extends ServiceImpl<UserFriendMapper, UserFriend> {
+    public UserFriend getByFriend(Long uid, Long targetUid) {
+        return lambdaQuery()
+                .eq(UserFriend::getUid, uid)
+                .eq(UserFriend::getFriendUid, targetUid)
+                .one();
+    }
 }
---
+++ yanchat-chat-server/src/main/java/com/yan/yanchat/common/user/mapper/UserFriendMapper.java
@@ -0,0 +1,13 @@
+package com.yan.yanchat.common.user.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.yan.yanchat.common.user.domain.entity.UserFriend;
+
+/**
+ * @Author: sixcolor
+ * @Date: 2024-02-18
+ * @Description: 用户联系人表 Mapper 接口
+ */
+public interface UserFriendMapper extends BaseMapper<UserFriend> {
+
+}
