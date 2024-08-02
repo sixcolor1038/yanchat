50e2981d132f535546738bc32bfbb29d40f3b34a
sixcolor <sixcolor1038@163.com>
2024-08-03 07:45:48 +0800

    消息模块开发

---
+++ yanchat-chat-server/src/main/java/com/yan/yanchat/common/infrastructure/domain/enums/YesOrNoEnum.java
@@ -0,0 +1,39 @@
+package com.yan.yanchat.common.infrastructure.domain.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+/**
+ * @Author: sixcolor
+ * @Date: 2024-02-16
+ * @Description:
+ */
+@Getter
+@AllArgsConstructor
+public enum YesOrNoEnum {
+    YES(1, "是"),
+    NO(0, "否");
+
+
+    private final Integer status;
+    private final String desc;
+
+    private static Map<Integer, YesOrNoEnum> cache;
+
+    static {
+        cache = Arrays.stream(YesOrNoEnum.values()).collect(Collectors.toMap(YesOrNoEnum::getStatus, Function.identity()));
+    }
+
+    public static YesOrNoEnum of(Integer type) {
+        return cache.get(type);
+    }
+
+    public static Integer toStatus(Boolean bool) {
+        return bool ? YES.getStatus() : NO.getStatus();
+    }
+}
