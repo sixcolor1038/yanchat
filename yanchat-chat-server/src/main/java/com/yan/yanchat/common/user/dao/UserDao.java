63d6aee7858c7f6009e2aea0c91234b80da1e0fe
sixcolor <sixcolor1038@163.com>
2024-08-03 07:46:10 +0800

    消息模块开发

--- yanchat-chat-server/src/main/java/com/yan/yanchat/common/user/dao/UserDao.java
+++ yanchat-chat-server/src/main/java/com/yan/yanchat/common/user/dao/UserDao.java
@@ -1,7 +1,7 @@
 package com.yan.yanchat.common.user.dao;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import com.yan.yanchat.common.infrastructure.domain.enums.BaseEnum;
+import com.yan.yanchat.common.infrastructure.domain.enums.YesOrNoEnum;
 import com.yan.yanchat.common.user.domain.entity.User;
 import com.yan.yanchat.common.user.mapper.UserMapper;
 import org.springframework.stereotype.Service;
@@ -42,7 +42,7 @@ public class UserDao extends ServiceImpl<UserMapper, User> {
     public void invalidUid(Long uid) {
         lambdaUpdate()
                 .eq(User::getId, uid)
-                .set(User::getStatus, BaseEnum.YES.getStatus())
+                .set(User::getStatus, YesOrNoEnum.YES.getStatus())
                 .update();
     }
 
